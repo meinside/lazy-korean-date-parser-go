8b82ba8efa2657280ee910d9f59bf88a7b72cfd0
Sungjin Han <meinside@gmail.com>
2018-03-20 21:58:44 +0900

    Fix test

--- lkdp_test.go
+++ lkdp_test.go
@@ -44,6 +44,6 @@ func BenchmarkExtractDate(b *testing.B) {
 
 func BenchmarkExtractTime(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		_, _ = ExtractDate(`게임은 오후 7시 30분부터 시작했다. 아직 아내에게 혼날 정도로 오래 하진 않았다고 생각한다.`, true)
+		_, _, _, _ = ExtractTime(`게임은 오후 7시 30분부터 시작했다. 아직 아내에게 혼날 정도로 오래 하진 않았다고 생각한다.`, true)
 	}
 }
